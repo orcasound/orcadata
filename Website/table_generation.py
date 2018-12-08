1c7e8790a047e685426466d227aa1ef5e0c7be21
Scott Veirs <sveirs@gmail.com>
2018-12-08 00:13:45 -0800

    Add PNGs from Ford's old catalog. Finish table_generation.py first draft.

--- Website/table_generation.py
+++ Website/table_generation.py
@@ -1,41 +1,38 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep 22 14:49:00 2018
-
 @author: kmuss
+Modified on Fri Dec 07 23:18:00 2018
+@author: scottveirs
 """
+
 from os import listdir
 from os.path import isfile, join
 
 #Please update this to YOUR path to orcadata
-basePath = r"C:\Users\kmuss\Desktop"
+#basePath = r"C:\Users\kmuss\Desktop"
+basePath = r"/Users/scott/Dropbox/Scottcoding/Orcasound/"
 
 #plotPath = join(basePath, "orcadata", "spectrogram", "Plots")
 folderpath_flac = join(basePath, "orcadata", "Sounds", "catalog", "flac")
 onlyfiles = [f for f in listdir(folderpath_flac) if isfile(join(join(folderpath_flac, f)))]
-#folderpath_mp3 = join(basePath, "orcadata", "Sounds", "catalog", "mp3")
-#folderpath_ogg = join(basePath, "orcadata", "Sounds", "catalog", "ogg")
 htmlpath = join(basePath, "orcadata", "Website")
-github_link = "https://github.com/orcasound/orcadata/Sounds/catalog/"
-
+base_URL = "https://github.com/orcasound/orcadata/"
 
 f = open(join(htmlpath, "spectrum_comparison.html"), "w")
 f.write("<table>\n")
 for file in onlyfiles:
     file_name = file.split(".")[0]
     heading = file_name.split('-')[1]
-    f.write("<tr>\n")
-    f.write("<h3>" + heading + "<h3>")
-    f.write("<audio controls> <source src=" + github_link + "/mp3/" + file_name + ".mp3"
-            'type="audio/mp3">')
-    f.write("<source src=" + github_link + "/ogg/" + file_name + ".ogg"
-            'type="audio/ogg">'))
- 
-    """
-Still need to add 
-new spectrum 
-old spectrum 
-close <tr>
-close table 
+    f.write("<tr>\n<td>\n")
+    f.write("<h3>" + heading + "<h3>\n")
+    f.write("<audio controls>\n")
+    f.write("<source src=\"" + base_URL + "Sounds/catalog/mp3/" + file_name + ".mp3\" " 'type="audio/mp3">\n')
+    f.write("<source src=\"" + base_URL + "Sounds/catalog/ogg/" + file_name + ".ogg\" " 'type="audio/ogg">\n')
+    f.write("</audio>\n")
+    f.write("</td>\n")
+    f.write("<td> <img src=\"" + base_URL + "Spectrogram/Plots/" + file_name + ".png\"> </td>\n")
+    f.write("<td> <img src=\"" + base_URL + "Spectrogram/Plots-Ford/" + heading + ".png\"> </td>\n")
+    f.write("</tr>\n")
+f.write("</table>\n")
 
-"""   
