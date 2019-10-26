1361809601238c3b74fadb69413af645952e487e
John Thickstun <thickstn@kakade-gpu2.cs.washington.edu>
2019-10-26 14:30:46 -0700

    don't restore checkpoint by default!

--- hackathonJuly27/lib/opt.py
+++ hackathonJuly27/lib/opt.py
@@ -11,13 +11,9 @@ def optimize(model, train_set, test_set, learning_rate=0.01, momentum=.95, batch
     worker_config['num_workers'] = workers
 
     train_loader = torch.utils.data.DataLoader(dataset=train_set,batch_size=batch_size,shuffle=True,**worker_config)
-
-    prng = np.random.RandomState(999)
     test_loader = torch.utils.data.DataLoader(dataset=test_set,batch_size=batch_size,**worker_config)
-    train_loader = torch.utils.data.DataLoader(dataset=train_set,batch_size=batch_size,**worker_config)
 
     print('Initiating optimizer, {} iterations/epoch.'.format(len(train_loader)))
-    model.restore_checkpoint()
     print(model.status_header())
 
     if alg == 'SGD':
@@ -32,18 +28,19 @@ def optimize(model, train_set, test_set, learning_rate=0.01, momentum=.95, batch
         for epoch in range(epochs):
             for i, (x,y) in enumerate(train_loader):
                 if i % update_rate == 0:
-                    with model.iterate_averaging():
-                        model.update_status(train_loader, test_loader, t, time())
+                    model.update_status(train_loader, test_loader, t, time())
                     model.checkpoint()
                     print(model.status())
                     t = time()
 
+                model.train()
                 optimizer.zero_grad()
                 x,y = model.prepare_data(x,y)
                 loss = model.loss(model(x,y),y)
                 loss.backward()
                 optimizer.step()
                 model.average_iterates()
+                model.eval()
 
     except KeyboardInterrupt:
         print('Graceful Exit')
