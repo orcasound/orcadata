784e77050d835d7ff82781290233d49390fb977d
John Thickstun <thickstn@kakade-gpu2.cs.washington.edu>
2019-10-27 20:57:28 -0700

    subsample for evaluation on train

--- hackathonJuly27/lib/opt.py
+++ hackathonJuly27/lib/opt.py
@@ -7,11 +7,15 @@ import torch
 
 from .dataset import worker_config
 
-def optimize(model, train_set, test_set, learning_rate=0.01, momentum=.95, batch_size=1000, epochs=40, workers=4, update_rate=1000, l2=0, alg='SGD'):
+def optimize(model, train_set, test_set, learning_rate=0.01, momentum=.95, batch_size=1000, epochs=40, workers=4, update_rate=1000, l2=0, alg='SGD', sample_size=20000):
     worker_config['num_workers'] = workers
 
     train_loader = torch.utils.data.DataLoader(dataset=train_set,batch_size=batch_size,shuffle=True,**worker_config)
     test_loader = torch.utils.data.DataLoader(dataset=test_set,batch_size=batch_size,**worker_config)
+
+    prng = np.random.RandomState(999)
+    train_sampler = torch.utils.data.sampler.SubsetRandomSampler(prng.choice(range(len(train_set)),min(sample_size,len(train_set)),replace=False))
+    train_sample_loader = torch.utils.data.DataLoader(dataset=train_set,batch_size=batch_size,sampler=train_sampler,**worker_config)
 
     print('Initiating optimizer, {} iterations/epoch.'.format(len(train_loader)))
     print(model.status_header())
@@ -28,7 +32,7 @@ def optimize(model, train_set, test_set, learning_rate=0.01, momentum=.95, batch
         for epoch in range(epochs):
             for i, (x,y) in enumerate(train_loader):
                 if i % update_rate == 0:
-                    model.update_status(train_loader, test_loader, t, time())
+                    model.update_status(train_sample_loader, test_loader, t, time())
                     model.checkpoint()
                     print(model.status())
                     t = time()
