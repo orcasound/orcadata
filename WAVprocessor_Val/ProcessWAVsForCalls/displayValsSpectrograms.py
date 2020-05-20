7c05c8802078652ac9af30bbc547de2aa8d49be2
val veirs <vveirs@coloradocollege.edu>
2020-05-20 09:51:52 -0700

    Add files via upload

---
+++ WAVprocessor_Val/ProcessWAVsForCalls/displayValsSpectrograms.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 08:38:29 2020
+
+@author: val
+"""
+import os
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pickle
+
+
+#### Directory where the pickled spectrograms and associated metadata will be stored
+pklDirectory = "/home/val/Documents/machineLearning/NN_files/dataForNN/pklFiles/"  
+
+
+def save_obj(obj, name ):
+    print("in save_obj_",os.getcwd(),name)
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    print("Working directory is ",os.getcwd())
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)  
+      
+class acousticLabel(object):
+  def __init__(self, fileline, thisClass,Nfft, sampleRate, n_bands, n_slices, flow, fhigh, dt, rmclicks, logScale, thisDate):
+    if '\t' in fileline:
+      items = fileline.split("\t")
+    items = fileline.split(",")  
+    self.objectClass = thisClass
+    self.wav_filename = items[0]
+    self.Nfft = Nfft,
+    self.sampleRate = sampleRate
+    self.start_time_s = float(items[1])
+    self.duration_s = float(items[2])
+    self.location = items[3]
+    self.window_start_idx = 0
+    self.window_stop_idx = 0
+    self.spectrogramType = ""
+    self.n_bands = n_bands
+    self.n_slices = n_slices
+    self.f_low = flow
+    self.f_high = fhigh
+    self.deltaT = dt
+    self.logFrequencyScale = logScale
+    self.removeClicks = rmclicks
+    self.processingDate = thisDate
+   
+    
+
+def plotArray(theTitle, z):
+  global n_bands, n_slices
+  y, x = np.mgrid[slice(1, n_bands + 1, 1),
+                slice(1, n_slices + 1, 1)]
+  z = z[:-1, :-1]
+  
+  # ##opening figure and axes
+  fig,ax = plt.subplots()
+
+  matplotlib.pyplot.imshow(z,origin='lower')
+  ax.set_title(theTitle)    
+  
+  plt.show() 
+
+####################################### EXECUTION STARTS HERE ######################
+    
+os.chdir(pklDirectory) 
+    
+signalArrayList = load_obj("signalArrayList_80x80_400to10000_3.0sec")  # Note: don't include the .pkl in the file name
+signalLabelList = load_obj("signalLabelList_80x80_400to10000_3.0sec")  
+
+n_bands = 80
+n_slices = 80
+
+for i in range(len(signalArrayList)):
+  signal = signalArrayList[i]
+  label  = signalLabelList[i]
+  exStop = label.start_time_s + label.duration_s
+  winStart = label.window_start_idx / label.sampleRate
+  winStop =  label.window_stop_idx / label.sampleRate
+  title = "Signal: %s\n%s\nExpert %0.1f,%0.1f Display %0.1f,%0.1f" % (label.wav_filename, label.spectrogramType,label.start_time_s, exStop, winStart, winStop)
+  plotArray(title,signal)
+  key = input("<Enter> to continue, q<Enter> to quit")
+  if key == 'q' or key == 'Q':
+    break
---
+++ WAVprocessor_Val/ProcessWAVsForCalls/readme.md
@@ -0,0 +1,47 @@
+README.md
+
+Val’s WAV and Labels Preprocessor demonstrates selecting labeled segments of WAV files, calculating the power spectral density for these segments, compressing the frequency spectrum into a selected range with a selected spectral resolution.  The amplitude to the psd in the final compressed spectrogram is normalized to a maximum of 1.  The frequency scale can be linear or may be set to be logarithmic.  The time axis is sampled to cover as selected time interval with a selected number of power spectral density segments.
+
+### To be added
+Each labeled segment is sampled at a selected number of time intervals close to the labeled time.  This generates spectrograms with the signal at a number of randomly selected times close to the labeled time.
+### To be added
+In addition, an equal number of ‘background’ spectrograms are computed at times randomly selected between the times of labeled segments.
+ 
+The final spectrograms are stored (pickled) in binary form as numpy arrays.  
+
+Python script:  ValsWavAndLabelsPreprocessor_0.5.py
+
+Here are parameters users may set:
+
+Input data files:
+
+#### Directory where the wav files are stored
+wavFileDirectory = "/home/val/Documents/machineLearning/Round3_OS_09_27_2017/wav/"
+
+#### Label file
+labelFilename = "/home/val/Documents/machineLearning/Round3_OS_09_27_2017/train.tsv"
+
+#### Directory where the pickled spectrograms and associated metadata will be stored
+pklDirectory = "/home/val/Documents/machineLearning/NN_files/dataForNN/pklFiles/"  
+
+User parameters:
+Nfft = 512		#resolution of initial psd calculation
+n_slices = 80		#number of time segments in spectrogram (number of ‘pixels’ along the x-axis)
+n_bands = 80		#number of frequency segments in spectrogram (number of ‘pixels’ along y)
+f_low = 400		#low frequency cutoff 
+f_high  = 8000		#high frequency cutoff
+deltaT = 3		#number of seconds along the x-axis of spectrogram
+n_specsPerLabel = 4	#number of spectrograms randomly calculated around labelled time
+logFrequency = False 	#True here integrates psd into n_bands logarithmic between f_low and f_high
+removeClicks = True  	#simple click remover is applied to wav file data before psd calculation
+showPlots = True        #show spectrograms 
+pauseAtEachPlot = True  #wait for user after each plot
+
+
+Python script:  displayValsSpectrograms.py 
+A program to display image representations of stored spectrograms.
+
+Program design:
+
+Print out summary of data file directories and summary of user parameters.
+Read label file and construct list of acousticLable objects. 
